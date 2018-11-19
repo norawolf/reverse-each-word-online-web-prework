80dea868c3943888eb0ce64f9e6dfe7bdb032814
Nora Vogt <noraevogt@gmail.com>
2018-11-19 20:50:38 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+def reverse_each_word(sentence)
+  new_sentence = []
+  sentence.split(" ").each do |word|
+    new_sentence << word.reverse
+  end
+  new_sentence.join(" ")
+end
+
+def reverse_each_word(sentence)
+  sentence.split(" ").collect do |word|
+      sentence << word.reverse!
+    end
+  end
