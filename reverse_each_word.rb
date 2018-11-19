88af840ee4cb5c8e32c5fdf20abeb7cd4f7e2f79
Nora Vogt <noraevogt@gmail.com>
2018-11-19 20:55:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,10 +7,10 @@ def reverse_each_word(sentence)
 end
 
 def reverse_each_word(sentence)
- sentence.split(" ").collect {|x| x.reverse!}.join(" ")
-#  sentence.split(" ").collect do |word|
-#      word.reverse!
-#    end
-  #  sentence.join(" ")
+  new_sentence = []
+  sentence.split(" ").collect do |word|
+    new_sentence << word.reverse!
+  end
+  new_sentence.join(" ")
 
   end
